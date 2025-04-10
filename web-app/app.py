1d1491ad67c3ba6d240c700f9c4ad0ba7efecc8c
melissalkelly <melissalkelly1@gmail.com>
2025-04-09 23:19:36 -0400

    Update app.py

--- web-app/app.py
+++ web-app/app.py
@@ -201,9 +201,9 @@ def create_app():
         food_to_category = {}
         for category, foods in CATEGORIES.items():
             for food in foods:
-                food_to_category[food.lower()] = (
-                    category  # Ensure the mapping is case-insensitive
-                )
+                food_to_category[
+                    food.lower()
+                ] = category  # Ensure the mapping is case-insensitive
 
         # Categorizing the food items
         categorized = defaultdict(list)
